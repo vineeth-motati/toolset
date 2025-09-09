89b240f7b6f5e1cbac2b3311c601d4518e3061b4
vineeth-motati <vineethdec06@gmail.com>
2025-09-09 18:36:03 +0530

    fix: add Google Ads script to the head section in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,6 +19,13 @@ export default defineNuxtConfig({
                     content: 'width=device-width, initial-scale=1',
                 },
             ],
+            script: [
+                {
+                    async: true,
+                    src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7238618124751862',
+                    crossorigin: 'anonymous',
+                },
+            ],
         },
     },
 
